fc52da05e12bed30d43ce338bc07bc3bb396fe6c
Sai Okabayashi <saipuck@gmail.com>
2014-07-15 15:21:55 -0500

    through part 6

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -11,6 +11,8 @@ $(document).ready( function() {
   // Populate the user table on initial page load
   populateTable();
 
+  // Username link click
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 // Functions
@@ -21,6 +23,9 @@ function populateTable() {
 
   // jQuery AJAX call for JSON
   $.getJSON( '/users/userlist', function( data ) {
+
+    // user data array into global object userListData
+    userListData = data;
 
     // for each item in JSON, add a table row & cell to content
     $.each(data, function(){
@@ -34,4 +39,27 @@ function populateTable() {
     // inject into existing HTML table
     $( '#userList table tbody' ).html( tableContent );
   });
+};
+
+// Show User Info
+function showUserInfo(event) {
+
+  // Prevent Link from Firing
+  event.preventDefault();
+
+  // Retrieve username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // Get Index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  // Get our User Object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populate Info Box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+
 };
